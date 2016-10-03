58f39c1c0c87606fc7c104157bbe8af006ece13a
Shelley Hicklin <shelleyhicklin@hotmail.com>
2016-10-03 19:28:59 -0400

    Add project files.

---
+++ WebApplication3/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication3.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
+
+//extra comments for commit
